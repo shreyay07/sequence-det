a5f459f3354fd45f0f80cd4f38cf9a240207cbf4
Shreya Yadav <shreyaydv007@gmail.com>
2025-07-25 14:25:53 +0530

    Update seq_det_1011.v

--- src/seq_det_1011.v
+++ src/seq_det_1011.v
@@ -2,9 +2,9 @@ module seq_det_1011(
 	input clk,rst,in,
 	output reg out);
 
-parameter S0=2'b00, S1=2'b01, S2=2'b10, S3=2'b11;
+parameter S0=3'b000, S1=3'b001, S2=3'b010, S3=3'b011, S4=3'b100;
 
-reg [1:0] cur_state, next_state;
+reg [2:0] cur_state, next_state;
 
 always @(posedge clk or negedge rst)begin
 	if(!rst)
@@ -18,13 +18,14 @@ always @(*) begin
 		S0: next_state= in? S1:S0;
 		S1: next_state= in? S1:S2;
 		S2: next_state= in? S3:S0;
-		S3: next_state= in? S1:S2;
+		S3: next_state= in? S4:S2;
+		S4: next_state= in? S1:S2;
 		default: next_state= S0;
 	endcase
 end
 
 always @(*) begin
-        out<=(cur_state==S3)&&(next_state==S1);
+	out<=(cur_state==S4);
 end
 
 endmodule
